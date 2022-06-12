7f602c2da6d40557853b5d89c0d073b7e94478c8
khongtrunght <khongtrunght@gmail.com>
2022-06-12 22:14:31 +0700

    merge with heroku

--- app/index.py
+++ app/index.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from pydantic import BaseSettings
 from server.routers import auth_router, rating_router, operation_router
+from server.config.db import close_db
 
-app = FastAPI()
+class Settings(BaseSettings):
+    openapi_url: str = "/openapi.json"
 
-origins = ['https://localhost:8000']
+settings = Settings()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
+middleware = [ Middleware(CORSMiddleware, allow_origins=[''], allow_credentials=True, allow_methods=[''], allow_headers=['*'])]
+
+app = FastAPI(middelware = middleware)
+
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse("/docs")
+
 
 app.include_router(auth_router.router, prefix='/auth')
 app.include_router(rating_router.router, prefix='/rating')
 app.include_router(operation_router.router, prefix='/operation')
+
+app.add_event_handler("shutdown", close_db)
